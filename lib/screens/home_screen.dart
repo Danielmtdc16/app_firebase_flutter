ef57f1110d36561a3e6098441583234e3e0dc65b
Danielmtdc16 <danielmtdc16@gmail.com>
2024-03-26 20:52:03 -0300

    iniciando a criação do showModalBottomSheet para cadastro das horas.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:app_firebase_flutter/components/menu.dart';
 import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -33,6 +34,100 @@ class _HomeScreenState extends State<HomeScreen> {
         onPressed: () {},
         child: Icon(Icons.add),
       ),
+    );
+  }
+
+  showFormModal({Hour? model}) {
+    String title = "Adicionar";
+    String confirmationButton = "Salvar";
+    String skipButton = "Cancelar";
+
+    TextEditingController _dateController = TextEditingController();
+    final dateMaskFormatter = MaskTextInputFormatter(mask: '##/##/####');
+    TextEditingController _minutesController = TextEditingController();
+    final minutesMaskFormatter = MaskTextInputFormatter(mask: '##:##');
+    TextEditingController _descriptionController = TextEditingController();
+
+    if (model != null) {
+      title = "Editando";
+      _dateController.text = model.date;
+      _minutesController.text = HourHelper.minutesToHours(model.minutes);
+      if (model.descricao != null) {
+        _descriptionController.text = model.description!;
+      }
+    }
+
+    showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+        top: Radius.circular(24),
+      )),
+      builder: (context) {
+        return Container(
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.all(32),
+          child: ListView(
+            children: [
+              Text(
+                title,
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+              TextFormField(
+                controller: _dateController,
+                keyboardType: TextInputType.datetime,
+                decoration:
+                    InputDecoration(hintText: '26/03/2024', labelText: 'Data'),
+                inputFormatters: [dateMaskFormatter],
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                controller: _minutesController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    hintText: '00:00', labelText: 'Horas Trabalhadas'),
+                inputFormatters: [minutesMaskFormatter],
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                controller: _descriptionController,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  hintText: 'Lembrete do que você fez',
+                  labelText: 'Horas trabalhadas',
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(skipButton),
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                        
+                    },
+                    child: Text(confirmationButton),
+                  )
+                ],
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
